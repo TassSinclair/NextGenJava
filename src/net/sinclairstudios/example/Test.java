62f145eac9a5cf7671066baafbcaa14be6fb0cbb
Andrew Hamilton <andrew@sinclairstudios.net>
2015-08-30 14:36:37 +1200

    Recommitting blank Test page.

--- src/net/sinclairstudios/example/Test.java
+++ src/net/sinclairstudios/example/Test.java
@@ -1,19 +1,9 @@
 package net.sinclairstudios.example;
 
-import java.util.*;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 public class Test {
 
   public static void main(String... args) {
-
-
-    "blah blah".toUpperCase();
-
-//    (String::toUpperCase).apply("blah blah")
-    String fourthr33thr33 = String.valueOf(433);
+    
   }
 
 }
